da26ab8ad191526a4f8d08420244c70ccd3000de
ddomingofernandez <danieldomingofernandez@hotmail.com>
2018-02-14 15:41:00 +0100

    Revert "Bump version: 0.0.1 → 0.0.2-dev"
    This reverts commit 136e01ebf687702d6e4bfb82cf1f146f9233cbc3.

--- src/bio2bel_wikipathways/__init__.py
+++ src/bio2bel_wikipathways/__init__.py
@@ -4,7 +4,7 @@
 
 from .manager import Manager
 
-__version__ = '0.0.2-dev'
+__version__ = '0.0.1'
 
 __title__ = 'bio2bel_wikipathways'
 __description__ = "A package for converting Wikipathways to BEL"
