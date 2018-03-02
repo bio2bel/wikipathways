bc59b757dbd14bf42b5f5e4112cbd36d9ac9c67d
ddomingofernandez <danieldomingofernandez@hotmail.com>
2018-03-02 09:45:19 +0100

    Bump version: 0.0.2-dev → 0.0.2

--- src/bio2bel_wikipathways/__init__.py
+++ src/bio2bel_wikipathways/__init__.py
@@ -15,7 +15,7 @@ Citation
 """
 from .manager import Manager
 
-__version__ = '0.0.2-dev'
+__version__ = '0.0.2'
 
 __title__ = 'bio2bel_wikipathways'
 __description__ = "A package for converting Wikipathways to BEL"
