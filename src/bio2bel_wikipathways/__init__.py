b6004194e768ced47e5be8b2d7fcdbd18ee9da7d
Charles Hoyt <cthoyt@microvac.speedport.ip>
2018-06-01 19:25:23 +0200

    Bump version: 0.0.11-dev → 0.0.11

--- src/bio2bel_wikipathways/__init__.py
+++ src/bio2bel_wikipathways/__init__.py
@@ -15,7 +15,7 @@ Citation
 """
 from .manager import Manager
 
-__version__ = '0.0.11-dev'
+__version__ = '0.0.11'
 
 __title__ = 'bio2bel_wikipathways'
 __description__ = "A package for converting Wikipathways to BEL"
