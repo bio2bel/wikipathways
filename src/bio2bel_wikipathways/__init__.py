8a7baaab022dca90a55609bda18915849935b562
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-07-27 10:51:59 +0200

    Bump version: 0.0.13-dev → 0.1.0-dev

--- src/bio2bel_wikipathways/__init__.py
+++ src/bio2bel_wikipathways/__init__.py
@@ -15,7 +15,7 @@ Citation
 """
 from .manager import Manager
 
-__version__ = '0.0.13-dev'
+__version__ = '0.1.0-dev'
 
 __title__ = 'bio2bel_wikipathways'
 __description__ = "A package for converting Wikipathways to BEL"
