81d6b0a9727a8cb2c92873d9bf13df0af1c7cbb5
Charles Tapley Hoyt <cthoyt@wlan-224.scai.fraunhofer.de>
2018-07-31 13:06:22 +0200

    Bump version: 0.1.0-dev → 0.1.0

--- src/bio2bel_wikipathways/__init__.py
+++ src/bio2bel_wikipathways/__init__.py
@@ -21,7 +21,7 @@ Citation
 from .manager import Manager  # noqa: F401
 from .utils import get_version  # noqa: F401
 
-__version__ = '0.1.0-dev'
+__version__ = '0.1.0'
 
 __title__ = 'bio2bel_wikipathways'
 __description__ = "A package for converting Wikipathways to BEL"
